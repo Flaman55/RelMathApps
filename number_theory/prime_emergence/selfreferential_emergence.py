060479e83041c6b708d402e06e61d86051a472a1
Artur Flamandzki <flamandzki.artur@gmail.com>
2025-11-12 13:42:06 +0100

    Deleted unused lib

--- number_theory/prime_emergence/selfreferential_emergence.py
+++ number_theory/prime_emergence/selfreferential_emergence.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: PolyForm-Noncommercial-1.0.0
 # Copyright (c) 2025 Artur Flamandzki
 
-import random
 from typing import List
 
 # -------------------------------------------------------------
